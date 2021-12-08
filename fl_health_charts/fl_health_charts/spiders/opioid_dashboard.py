b533b9f1fb56bee275c03322d366a52e06c13c8e
Nick Anthony <nanthony007@gmail.com>
2021-12-08 12:45:26 -0500

    Add opioid_dashboard spider.

--- fl_health_charts/fl_health_charts/spiders/opioid_dashboard.py
+++ fl_health_charts/fl_health_charts/spiders/opioid_dashboard.py
@@ -1,10 +1,27 @@
 import scrapy
+from . import utils
+import json
+
+urls = utils.read_urls()
 
 
 class OpioidDashboardSpider(scrapy.Spider):
-    name = 'opioid_dashboard'
-    allowed_domains = ['https://flhealthcarts.gov']
-    start_urls = ['http://https://flhealthcarts.gov/']
+    name = "opioid_dashboard"
+    start_urls = urls
 
     def parse(self, response):
-        pass
+        county_info = utils.extract_county_name_and_year(response.url)
+        headers: list[str] = response.css("#dtSubstanceUse thead th::text").getall()
+        with open(f"data/{county_info['name']}_{county_info['year']}.jsonl", "w") as f:
+            for row in response.css("tr[row]"):
+                # gets only for the row so length varies (1-10ish)
+                row_data: list[str] = []
+                for cell in row.css("td"):
+                    cell_text = cell.css("span[id]::text").get(default="")
+                    row_data.append(cell_text)
+                # zip row data to headers
+                zipped_data: dict[str, str] = {
+                    x[0]: x[1] for x in zip(headers, row_data)
+                }
+                f.write(json.dumps(zipped_data))
+                f.write("\n")
